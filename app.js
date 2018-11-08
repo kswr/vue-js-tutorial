a54aaf58c41df62e82ecd6f9e96cd500531f3983
kswr <ksawery.oleszynski@gmail.com>
2018-11-08 15:40:33 +0100

    finished lesson

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ new Vue({
     el: '#vue-app',
     data: {
         health: 100,
+        ended: false,
     },
     methods: {
         punch: function () {
             this.health -= 10;
+            if ( this.health <=0 ) {
+                this.ended = true;
+            }
         },
         restart: function () {
             this.health = 100;
-        }
+            this.ended = false;
+        },
     },
     computed: {
-
+        computedWidth: function () {
+            return this.health * 2 + 'px';
+        }
     }
 });
