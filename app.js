950f505e9692c559ec92005a65db8ec9b9547531
kswr <ksawery.oleszynski@gmail.com>
2018-11-08 16:31:07 +0100

    finished lesson 15 Intro to components

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-var one = new Vue({
-    el: '#vue-app',
+Vue.component('greeting',{
+    template:'<p>Hey there, I am a {{name}}. <button v-on:click="changeName">Change name</button></p>',
+    data: function () {
+        return {
+            name: 'Yoshi',
+        }
+    },
+    methods: {
+        changeName: function () {
+            this.name = 'Mario';
+        }
+    }
+});
+
+new Vue({
+    el: '#vue-app-one',
     data: {
     },
     methods: {
@@ -8,3 +22,12 @@ var one = new Vue({
     }
 });
 
+new Vue({
+    el: '#vue-app-two',
+    data: {
+    },
+    methods: {
+    },
+    computed: {
+    }
+});
