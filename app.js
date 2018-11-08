7dfca1b589ddaa6ae8e8ef30d6b0e2f9b56b2fcb
kswr <ksawery.oleszynski@gmail.com>
2018-11-08 14:55:29 +0100

    added basic features

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 new Vue({
     el: '#vue-app',
     data: {
+        health: 100,
     },
     methods: {
-
+        punch: function () {
+            this.health -= 10;
+        },
+        restart: function () {
+            this.health = 100;
+        }
     },
     computed: {
 
