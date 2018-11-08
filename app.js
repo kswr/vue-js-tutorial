a429d37a4207528f31da68df08c2c53d4707a03f
kswr <ksawery.oleszynski@gmail.com>
2018-11-08 11:54:38 +0100

    start conditionals

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 new Vue({
     el: '#vue-app',
     data: {
-        available: false,
-        nearby: false,
     },
     methods: {
 
     },
     computed: {
-        compClasses: function () {
-            return {
-                available: this.available,
-                nearby: this.nearby
-            }
-        }
+
     }
 });
