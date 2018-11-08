72c642b8bddaea6fa8b77c0159facf4a3e000082
kswr <ksawery.oleszynski@gmail.com>
2018-11-08 16:08:00 +0100

    finished lesson 14

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-new Vue({
-    el: '#vue-app',
+var one = new Vue({
+    el: '#vue-app-one',
     data: {
+        title: 'Vue App One'
     },
     methods: {
     },
     computed: {
+        greet: function () {
+            return 'Hello from app one'
+        }
     }
 });
+
+var two = new Vue({
+    el: '#vue-app-two',
+    data: {
+        title: 'Vue App Two'
+    },
+    methods: {
+        changeTitle: function () {
+            one.title = 'Title changed from app Two'
+        }
+    },
+    computed: {
+        greet: function () {
+            return 'This is app2 speaking'
+        }
+    }
+});
+
+two.title = 'Changed from outside';
