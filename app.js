f1767a008957631fcb23cfd9f4e7b2907b7e1b14
kswr <ksawery.oleszynski@gmail.com>
2018-11-08 16:10:43 +0100

    branched to 015-intro-to-components

--- app.js
+++ app.js
@@ -1,32 +1,10 @@
 var one = new Vue({
-    el: '#vue-app-one',
+    el: '#vue-app',
     data: {
-        title: 'Vue App One'
     },
     methods: {
     },
     computed: {
-        greet: function () {
-            return 'Hello from app one'
-        }
     }
 });
 
-var two = new Vue({
-    el: '#vue-app-two',
-    data: {
-        title: 'Vue App Two'
-    },
-    methods: {
-        changeTitle: function () {
-            one.title = 'Title changed from app Two'
-        }
-    },
-    computed: {
-        greet: function () {
-            return 'This is app2 speaking'
-        }
-    }
-});
-
-two.title = 'Changed from outside';
