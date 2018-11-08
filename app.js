0ce391a90f255a667757c364c2967237e7c88cf7
kswr <ksawery.oleszynski@gmail.com>
2018-11-08 16:33:06 +0100

    branched into 016-referencing-ref

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
-Vue.component('greeting',{
-    template:'<p>Hey there, I am a {{name}}. <button v-on:click="changeName">Change name</button></p>',
-    data: function () {
-        return {
-            name: 'Yoshi',
-        }
-    },
-    methods: {
-        changeName: function () {
-            this.name = 'Mario';
-        }
-    }
-});
 
 new Vue({
-    el: '#vue-app-one',
-    data: {
-    },
-    methods: {
-    },
-    computed: {
-    }
-});
-
-new Vue({
-    el: '#vue-app-two',
+    el: '#vue-app',
     data: {
     },
     methods: {
