dcecd4172a38ae3191394c159fc0ae7f6721b1f9
kswr <ksawery.oleszynski@gmail.com>
2018-11-09 16:58:09 +0100

    finished lesson 19

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,28 @@
 <template>
   <div>
     <h1>{{title}}</h1>
-    <p>{{greeting()}}</p>
+    <ninjas></ninjas>
   </div>
 </template>
 
 <script>
+import Ninjas from './Ninjas.vue'
+
+
 export default {
+    components: {
+      'ninjas':Ninjas
+    },
   data () {
     return {
-      title: 'Your first vue file'
+      title: 'Ninja App'
     }
   },
   methods: {
-    greeting: function () {
-      return 'hello';
-    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
---
+++ src/Ninjas.vue
@@ -0,0 +1,19 @@
+<template xmlns:v-for="http://www.w3.org/1999/xhtml">
+  <ul>
+    <li v-for="ninja in ninjas"> {{ ninja }} </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        ninjas: ['Yoshi','Mario','Ryu']
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
