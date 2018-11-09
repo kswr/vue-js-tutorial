bcd47d66d419c6aad420b22b7b519dfc5cde7fcb
kswr <ksawery.oleszynski@gmail.com>
2018-11-09 21:36:20 +0100

    finished lesson 22

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <div id="app">
+  <div>
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+  import Header from './components/Header.vue';
+  import Footer from './components/Footer.vue';
+  import Ninjas from './components/Ninjas.vue';
+
 export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas,
+  },
   data () {
     return {
     }
