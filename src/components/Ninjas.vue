edf9f470a81e7d54e68b70f6adbe449de58dc1ae
kswr <ksawery.oleszynski@gmail.com>
2018-11-10 00:34:51 +0100

    finished 23 primitive vs reference type

--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -6,6 +6,7 @@
         <h3 v-show="ninja.show ">{{ ninja.speciality }}</h3>
       </li>
     </ul>
+    <button v-on:click="deleteNinja">Delete Ninja</button>
   </div>
 </template>
 
@@ -22,6 +23,9 @@
       }
     },
     methods: {
+      deleteNinja: function () {
+        this.ninjas.pop();
+      }
     }
   }
 </script>
