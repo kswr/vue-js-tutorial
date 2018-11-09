2ec57fd589ae5696c38a2143de023daecb0afe1b
kswr <ksawery.oleszynski@gmail.com>
2018-11-09 11:46:01 +0100

    finished lesson 16 referencing

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 new Vue({
     el: '#vue-app',
     data: {
+        output: 'your fav food'
     },
     methods: {
+        readRefs: function () {
+            console.log(this.$refs.test.innnerText);
+            this.output = this.$refs.input.value;
+        }
     },
     computed: {
     }
