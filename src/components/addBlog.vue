41871dbc9a4c25ffabd688fec1ff8ac652477f0d
kswr <ksawery.oleszynski@gmail.com>
2018-11-10 21:13:02 +0100

    finished lesson 30

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -6,12 +6,27 @@
       <input type="text" v-model="blog.title" required/>
       <label>Blog Content:</label>
       <textarea v-model="blog.content"></textarea>
+      <div id="checkboxes">
+        <label>Ninjas</label>
+        <input type="checkbox" value="ninjas" v-model="blog.categories"/>
+        <label>Wizards</label>
+        <input type="checkbox" value="wizards" v-model="blog.categories"/>
+        <label>Mario</label>
+        <input type="checkbox" value="mario" v-model="blog.categories"/>
+        <label>Cheese</label>
+        <input type="checkbox" value="cheese" v-model="blog.categories"/>
+      </div>
     </form>
     <div id="preview">
       <h3>Preview Blog</h3>
       <p>Blog title: {{ blog.title }}</p>
       <p>Blog content:</p>
       <p> {{ blog.content }}</p>
+      <p>Blog categories:</p>
+
+      <ul>
+        <li v-for="category in blog.categories">{{ category }}</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -23,7 +38,8 @@
       return {
         blog: {
           title: '',
-          content: ''
+          content: '',
+          categories: [],
         },
       }
     },
@@ -55,5 +71,13 @@
   }
   h3{
     margin-top: 10px;
+  }
+  #checkboxes input {
+    display: inline-block;
+    margin-right: 10px;
+  }
+
+  #checkboxes label {
+    display: inline-block;
   }
 </style>
