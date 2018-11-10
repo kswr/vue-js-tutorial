d5e0d3b31ed7c7f8f0e74d9dd9e6236ae7ccead3
kswr <ksawery.oleszynski@gmail.com>
2018-11-10 17:45:06 +0100

    finished lesson 26

--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -26,6 +26,25 @@
       deleteNinja: function () {
         this.ninjas.pop();
       }
+    },
+    // lifecycle hooks
+    beforeCreate(){
+      alert('beforeCreate');
+    },
+    created() {
+      alert('created');
+    },
+    beforeMount() {
+      alert('beforeMount');
+    },
+    mounted() {
+      alert('mounted');
+    },
+    beforeUpdate() {
+      alert('beforeUpdate');
+    },
+    updated() {
+      alert('updated');
     }
   }
 </script>
