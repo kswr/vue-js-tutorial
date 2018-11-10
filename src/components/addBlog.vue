27c0ae85a7e550dd8e5bb8d81eb61bf4d7afa759
kswr <ksawery.oleszynski@gmail.com>
2018-11-10 21:35:33 +0100

    finished lesson 31

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -16,7 +16,12 @@
         <label>Cheese</label>
         <input type="checkbox" value="cheese" v-model="blog.categories"/>
       </div>
+      <label>Author:</label>
+      <select v-model="blog.author">
+        <option v-for="author in authors"> {{ author }}</option>
+      </select>
     </form>
+
     <div id="preview">
       <h3>Preview Blog</h3>
       <p>Blog title: {{ blog.title }}</p>
@@ -27,6 +32,7 @@
       <ul>
         <li v-for="category in blog.categories">{{ category }}</li>
       </ul>
+      <p>Author: {{ blog.author }}</p>
     </div>
   </div>
 </template>
@@ -40,7 +46,9 @@
           title: '',
           content: '',
           categories: [],
+          author: '',
         },
+        authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator'],
       }
     },
   }
