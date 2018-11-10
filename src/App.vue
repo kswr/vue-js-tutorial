8b69ad3faaf7bffadad370666f5659f4eabdedf7
kswr <ksawery.oleszynski@gmail.com>
2018-11-10 20:56:14 +0100

    finished lesson 29

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+  import addBlog from './components/addBlog'
+
+export default {
+    components: {
+      'add-blog': addBlog,
+    },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+
+<style>
+  body {
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+  }
+</style>
