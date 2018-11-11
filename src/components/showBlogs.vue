e2d644805ab36292ac49d0fb47a2628096c1b1e9
kswr <ksawery.oleszynski@gmail.com>
2018-11-11 13:52:33 +0100

    finished lesson 33

---
+++ src/components/showBlogs.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <div v-for="blog in blogs" class="single-blog">
+      <h2>{{blog.title}}</h2>
+      <p>{{blog.body}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+    },
+    data () {
+      return {
+        blogs: [],
+      }
+    },
+    created() {
+      this.$http.get('http://jsonplaceholder.typicode.com/posts').then(
+        function (data) {
+          console.log(data);
+          this.blogs = data.body.slice(0,10);
+        }
+      )
+    }
+  }
+</script>
+
+
+<style scoped>
+  #show-blogs{
+    max-width: 800px;
+    margin: 0 auto;
+  }
+
+  .single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
+  }
+</style>
