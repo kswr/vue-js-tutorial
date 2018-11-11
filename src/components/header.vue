3aa500a238429b4e5798e1c376b1ef105e765f12
kswr <ksawery.oleszynski@gmail.com>
2018-11-11 19:38:40 +0100

    finished lesson 41

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,8 +1,8 @@
 <template>
     <nav>
       <ul>
-        <li><router-link to="/">Blog</router-link></li>
-        <li><router-link to="/add">Add a new blog</router-link></li>
+        <li><router-link to="/" exact>Blog</router-link></li>
+        <li><router-link to="/add" exact>Add a new blog</router-link></li>
       </ul>
     </nav>
 </template>
@@ -13,5 +13,28 @@
 </script>
 
 <style scoped>
-
+  ul{
+    list-style-type: none;
+    text-align: center;
+    margin: 0;
+  }
+  li{
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a{
+    color: #fff;
+    text-decoration: none;
+    padding: 6px 8px;
+    border-radius: 10px;
+  }
+  nav{
+    background: #444;
+    padding: 14px 0;
+    margin-bottom: 40px;
+  }
+  .router-link-active{
+    background: #eee;
+    color: #444;
+  }
 </style>
