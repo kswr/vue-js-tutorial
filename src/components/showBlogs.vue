78bbe30a600559479b65a77b8cf413d6b476eebd
kswr <ksawery.oleszynski@gmail.com>
2018-11-11 20:06:00 +0100

    finished lesson 42

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -3,7 +3,7 @@
     <h1>All Blog Articles</h1>
     <input id ="search" v-model="search" type="text" placeholder="search blogs"/>
     <div v-for="blog in filteredBlogs" class="single-blog">
-      <h2 v-rainbow>{{blog.title | to-uppercase }}</h2>
+      <router-link v-bind:to="/blog/ + blog.id"><h2>{{blog.title | to-uppercase }}</h2></router-link>
       <p>{{blog.body | snippet }}</p>
     </div>
   </div>
