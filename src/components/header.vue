ba601c871a7a58c5b53952744be69139ec8eebb9
kswr <ksawery.oleszynski@gmail.com>
2018-11-11 19:21:23 +0100

    finished lesson 40

---
+++ src/components/header.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav>
+      <ul>
+        <li><router-link to="/">Blog</router-link></li>
+        <li><router-link to="/add">Add a new blog</router-link></li>
+      </ul>
+    </nav>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+
+</style>
